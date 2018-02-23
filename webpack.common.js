68a21da107fe6ee83d6ca0557eac1e0aa7e7601d
zelinf <fengzlin@mail2.sysu.edu.cn>
2018-02-23 20:17:50 +0800

    Indent webpack config file with 2 spaces instead of 4

--- webpack.common.js
+++ webpack.common.js
@@ -3,50 +3,50 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/build"
-    },
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/build"
+  },
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: "resources/"
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
-                        },
-                    },
-                ],
-            }
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
         ]
-    },
+      },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: "resources/"
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            },
+          },
+        ],
+      }
+    ]
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' })
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
 
 };
